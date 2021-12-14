8dc130e3e1da6acfcf7d2bcacae402345198611a
lorenzo.pepe <lorenzo.pepe@hec.edu>
2021-12-14 20:39:56 +0100

    add src and plots script + fix extraction from more paths

---
+++ src/plots.py
@@ -0,0 +1,12 @@
+from matplotlib import pyplot as plt
+
+def plot_observation(images_lists, idobs):
+    fig, axs = plt.subplots(nrows=1, ncols=5, figsize=(12,4))
+    for i, ax in enumerate(axs.flatten()):
+        plt.sca(ax)
+        plt.imshow(images_lists[i][idobs]) #cmap=plt.cm.jet
+        #plt.colorbar()
+        plt.title('Image: {}'.format(i+1))
+
+    plt.suptitle('Overall Title')
+    plt.show()
