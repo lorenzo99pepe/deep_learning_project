5113e22874ed1bee8dea6c02bf96ca2cc2f7aa1f
lorenzo.pepe <lorenzo.pepe@hec.edu>
2021-12-15 11:48:24 +0100

    add finetuning main functions

---
+++ src/loading.py
@@ -0,0 +1,59 @@
+import numpy as np
+from PIL import Image
+from torch.utils.data import DataLoader
+from torchvision import transforms
+
+from src.segdataset import SegmentationDataset
+
+def get_dataloader_single_folder(data_dir: str,
+                                 image_folder: str = 'Images',
+                                 mask_folder: str = 'Masks',
+                                 fraction: float = 0.2,
+                                 batch_size: int = 4):
+    """Create train and test dataloader from a single directory containing
+    the image and mask folders.
+    Args:
+        data_dir (str): Data directory path or root
+        image_folder (str, optional): Image folder name. Defaults to 'Images'.
+        mask_folder (str, optional): Mask folder name. Defaults to 'Masks'.
+        fraction (float, optional): Fraction of Test set. Defaults to 0.2.
+        batch_size (int, optional): Dataloader batch size. Defaults to 4.
+    Returns:
+        dataloaders: Returns dataloaders dictionary containing the
+        Train and Test dataloaders.
+    """
+    data_transforms = transforms.Compose([transforms.ToTensor()])
+
+    image_datasets = {
+        x: SegmentationDataset(data_dir,
+                               image_folder=image_folder,
+                               mask_folder=mask_folder,
+                               seed=100,
+                               fraction=fraction,
+                               subset=x,
+                               transforms=data_transforms)
+        for x in ['Train', 'Test']
+    }
+    dataloaders = {
+        x: DataLoader(image_datasets[x],
+                      batch_size=batch_size,
+                      shuffle=True,
+                      num_workers=8)
+        for x in ['Train', 'Test']
+    }
+    return dataloaders
+
+
+def load_images_from_paths(input_path, seg_path):
+    used_files = sorted([f for f in input_path.iterdir()])
+    seg_files = sorted([f for f in seg_path.iterdir()])
+
+    images = []
+    segs = []
+    for i in range(len(used_files)):
+        images.append(np.asarray(Image.open(used_files[i])))
+        segs.append(np.asarray(Image.open(seg_files[i])))
+
+    images = np.array(images)
+    segs = np.array(segs)
+    return images, segs
--- src/modelling.py
+++ src/modelling.py
@@ -14,7 +14,47 @@ import numpy as np
 import torch
 from tqdm import tqdm
 
+from src.loading import get_dataloader_single_folder
+
 #import datahandler
+
+
+def deeplab_finetuning(data_directory, image_folder, mask_folder, exp_directory, epochs, batch_size):
+    # Create the deeplabv3 resnet101 model which is pretrained on a subset
+    # of COCO train2017, on the 20 categories that are present in the Pascal VOC dataset.
+    model = createDeepLabv3()
+    model.train()
+    data_directory = Path(data_directory)
+    # Create the experiment directory if not present
+    exp_directory = Path(exp_directory)
+    if not exp_directory.exists():
+        exp_directory.mkdir()
+
+    # Specify the loss function
+    criterion = torch.nn.MSELoss(reduction='mean')
+    # Specify the optimizer with a lower learning rate
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+
+    # Specify the evaluation metrics
+    metrics = {'f1_score': f1_score, 'auroc': roc_auc_score}
+
+    # Create the dataloader
+    dataloaders = get_dataloader_single_folder(
+        data_directory, 
+        image_folder=image_folder,
+        mask_folder=mask_folder,
+        batch_size=batch_size)
+        
+    _ = train_model(model,
+                    criterion,
+                    dataloaders,
+                    optimizer,
+                    bpath=exp_directory,
+                    metrics=metrics,
+                    num_epochs=epochs)
+
+    # Save the trained model
+    torch.save(model, exp_directory / 'weights.pt')
 
 
 def train_model(model, criterion, dataloaders, optimizer, metrics, bpath,
